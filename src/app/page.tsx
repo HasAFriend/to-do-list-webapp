46fc7d12aecc65d22d057222117ec3ce9170f0c1
inigog <inigogaston22@gmail.com>
2025-11-05 22:32:15 +0800

    Added background and text colors to globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-5xl font-main"> Hello world. </h1>
+    <div className="flex min-h-screen items-center justify-center font-main">
+      <h1 className="text-5xl"> Hello world. </h1>
     </div>
   );
 }
